bc19931a18a836c55ee5abb7e106d05e0b9087e3
Ahmad Jerjees <ajerjees010@gmail.com>
2024-12-02 06:46:51 +0300

    Updated number verification

--- PhoneBook/UserInterfaceControllers/AddContactMenu.cs
+++ PhoneBook/UserInterfaceControllers/AddContactMenu.cs
@@ -9,7 +9,6 @@ internal class AddContactMenu
 {
     private Contact _newContact;
     private PhoneBookContext PhoneBookDb { get; set; }
-    private ValidatePhoneNumber _validatePhone;
     public AddContactMenu(PhoneBookContext phoneBookDb)
     {
         PhoneBookDb = phoneBookDb;
@@ -24,16 +23,24 @@ internal class AddContactMenu
         _newContact.ContactName = AnsiConsole.Ask<string>("Contact Name:");
         _newContact.ContactTitle = AnsiConsole.Ask<string>("Contact Title E.g.(Family,Work,..):");
 
-        _newContact.ContactPhone = AnsiConsole.Ask<string>("Contact Phone:"); // set the new contact number status based on Number verifier 
-        _validatePhone = new("JO", _newContact.ContactPhone);
-        _newContact.ContactPhoneStatus = _validatePhone.IsPhoneNumberValid();
+        _newContact.ContactPhone = AnsiConsole.Ask<string>("Contact Phone Without Country Prefix:"); 
+        var countryCode = AnsiConsole.Ask<string>("Country Code (First two letters of your country name -Jordan = JO, United States = US)\nIF YOU ARE HAVING TROUBLE KNOWING YOUR COUNTRY CODE look it up on google:");
+        VerifiyPhoneNumber(countryCode);
 
-        _newContact.ContactEmail = AnsiConsole.Ask<string>("Contact Email:"); // set the new contact email status based on email verifier
+        _newContact.ContactEmail = AnsiConsole.Ask<string>("Contact Email:"); 
         _newContact.ContactEmailStatus = true;
 
         
         SaveNewContact();
         IndicateAddSuccess();
+    }
+
+    private void VerifiyPhoneNumber(string countryCode)
+    {
+        var validatePhone = new ValidatePhoneNumber(countryCode, _newContact.ContactPhone);
+        _newContact.ContactPhoneStatus = validatePhone.IsPhoneNumberValid();
+        string verification = validatePhone.IsPhoneNumberValid() ? "[green]Valid[/]" : "[red]Invalid[/]";
+        AnsiConsole.MarkupLine($"This number {_newContact.ContactPhone} is {verification}");
     }
 
     private void SaveNewContact()

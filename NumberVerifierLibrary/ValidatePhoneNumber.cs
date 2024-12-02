e954a4c91a66421145b34b55a29bb7cdb13c85cd
Ahmad Jerjees <ajerjees010@gmail.com>
2024-12-02 08:09:47 +0300

    Updated Phone verification

--- NumberVerifierLibrary/ValidatePhoneNumber.cs
+++ NumberVerifierLibrary/ValidatePhoneNumber.cs
@@ -1,4 +1,6 @@
-﻿namespace NumberVerifierLibrary;
+﻿using Spectre.Console;
+
+namespace NumberVerifierLibrary;
 
 public class ValidatePhoneNumber
 {
@@ -7,10 +9,24 @@ public class ValidatePhoneNumber
     private ValidatedPhoneNumber? _numberDetails;
     public ValidatePhoneNumber(string countryCode, string phoneNumber)
     {
-        _countryCode = "&country_code=" + countryCode;
-        _phoneNumber = "&number=" + phoneNumber;
+        _countryCode =   countryCode;
+        _phoneNumber =   phoneNumber;
     }
-
+    public bool IsPhoneNumberFormatValid()
+    {
+        if (!double.TryParse(_phoneNumber, out double _))
+        {
+            AnsiConsole.MarkupLine("[red]Invalid Phone Number-No characters allowed in the phone number![/]");
+            return false;
+        }
+        if (_phoneNumber.Length < 3 | _phoneNumber.Length > 17) // the only allowed range for any phone number
+        {
+            AnsiConsole.MarkupLine("[red]Invalid Phone Number-The entered number exceeds the allowed length![/]");
+            return false;
+        }
+        AnsiConsole.MarkupLine($"[green]Valid Phone Number Format!: {_phoneNumber}[/]");
+        return true;
+    }
     public bool IsPhoneNumberValid()
     {
         var task = LoadPhoneDetails();
@@ -24,7 +40,7 @@ public class ValidatePhoneNumber
         var client = httpRequestCreator.CreateHttpClient();
         var apiKey = httpRequestCreator.LoadApiKey();
         var requestUri = httpRequestCreator.LoadHttpValidationConnectionString();
-        var fullUri = requestUri + apiKey + _phoneNumber + _countryCode;
+        var fullUri = requestUri + apiKey + "&number=" + _phoneNumber + "&country_code=" + _countryCode;
 
         _numberDetails = await ApiRequest.ProcessRequestAsync<ValidatedPhoneNumber>(client, fullUri);
     }
--- PhoneBook/UserInterfaceControllers/AddContactMenu.cs
+++ PhoneBook/UserInterfaceControllers/AddContactMenu.cs
@@ -17,30 +17,35 @@ internal class AddContactMenu
 
     public void PromptNewContact()
     {
-        
+
         AnsiConsole.MarkupLine("[bold green]New Contact\n[/]");
 
         _newContact.ContactName = AnsiConsole.Ask<string>("Contact Name:");
         _newContact.ContactTitle = AnsiConsole.Ask<string>("Contact Title E.g.(Family,Work,..):");
 
-        _newContact.ContactPhone = AnsiConsole.Ask<string>("Contact Phone Without Country Prefix:"); 
+        _newContact.ContactPhone = AnsiConsole.Ask<string>("Contact Phone Without Country Prefix:");
         var countryCode = AnsiConsole.Ask<string>("Country Code (First two letters of your country name -Jordan = JO, United States = US)\nIF YOU ARE HAVING TROUBLE KNOWING YOUR COUNTRY CODE look it up on google:");
-        VerifiyPhoneNumber(countryCode);
+        _newContact.ContactPhoneStatus = VerifiyPhoneNumber(countryCode);
 
-        _newContact.ContactEmail = AnsiConsole.Ask<string>("Contact Email:"); 
+        _newContact.ContactEmail = AnsiConsole.Ask<string>("Contact Email:");
         _newContact.ContactEmailStatus = true;
 
-        
+
         SaveNewContact();
         IndicateAddSuccess();
     }
 
-    private void VerifiyPhoneNumber(string countryCode)
+    private bool VerifiyPhoneNumber(string countryCode)
     {
         var validatePhone = new ValidatePhoneNumber(countryCode, _newContact.ContactPhone);
-        _newContact.ContactPhoneStatus = validatePhone.IsPhoneNumberValid();
+
+        if (!validatePhone.IsPhoneNumberFormatValid()) // if the format isnt valid instantly invalidate the number and set status as unverified
+            return false;
+            
+        // if the format was valid , then check for the api if the number really exists and set the status accordingly
         string verification = validatePhone.IsPhoneNumberValid() ? "[green]Valid[/]" : "[red]Invalid[/]";
         AnsiConsole.MarkupLine($"This number {_newContact.ContactPhone} is {verification}");
+        return validatePhone.IsPhoneNumberValid();
     }
 
     private void SaveNewContact()
